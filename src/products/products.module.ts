296a26231c7a517d0154a5131c618b7f8013813c
Isa Mehmeti <isa-mehmeti@hotmail.com>
2024-07-01 12:06:54 +0200

    Remove ProductsController

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 import { LangchainModule } from "src/langchain/langchain.module";
-import { ProductsController } from "./products.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Product, ProductSchema } from "./schemas/product.schema";
 
@@ -13,7 +12,6 @@ import { Product, ProductSchema } from "./schemas/product.schema";
         LangchainModule],
     providers: [ProductsService],
     exports: [ProductsService],
-    controllers: [ProductsController]
 })
 export class ProductsModule { }
 
